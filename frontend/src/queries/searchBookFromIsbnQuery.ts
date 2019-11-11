88a888d3ed5559b7b031e3865553bebfb1af8c93
bon-chi <afterjnih@gmail.com>
2019-11-11 17:34:50 +0900

    add search book from isbn query

---
+++ frontend/src/queries/searchBookFromIsbnQuery.ts
@@ -0,0 +1,14 @@
+import {gql} from 'apollo-boost';
+
+export default gql`
+  query BookFromIsbnQuery($isbn: String!) {
+    bookFromIsbn(isbn: $isbn){
+      name
+      page
+      isbn{
+        code
+      }
+      dataSource
+    }
+  }
+`
